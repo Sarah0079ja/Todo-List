ac2d09c3c12d14fc642e7b7ebf1de137f1db976a
Sarah0079ja <sodiavbara@gmail.com>
2020-01-21 15:44:35 +0100

    Updates

---
+++ precache-manifest.6dd605c6e2cbec92f38b871fb11f9ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3dff3a6895be56d162572a4ee102d0",
+    "url": "/Todo-App-React/index.html"
+  },
+  {
+    "revision": "ed05434c25f026e258fd",
+    "url": "/Todo-App-React/static/css/main.2a025ad0.chunk.css"
+  },
+  {
+    "revision": "d328198509301bfb3221",
+    "url": "/Todo-App-React/static/js/2.5a623828.chunk.js"
+  },
+  {
+    "revision": "ed05434c25f026e258fd",
+    "url": "/Todo-App-React/static/js/main.619862d3.chunk.js"
+  },
+  {
+    "revision": "45befb460767ebcf9c50",
+    "url": "/Todo-App-React/static/js/runtime-main.63c9a226.js"
+  }
+]);
