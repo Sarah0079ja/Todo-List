5cac9302ec9bf1dba04804de7d7b93e6b1501513
Sarah0079ja <sodiavbara@gmail.com>
2020-01-21 15:03:47 +0100

    new-commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Header from './components/Header';
+import AddTodo from './components/AddTodo';
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Check your email',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Do stutern Assignment',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Go to the market',
+        completed: false
+      }
+    ]
+  }
+  //toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    })
+  }
+  //Delete  
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
+      </div>
+    );
+  }
+}
+export default App; 
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import Header from './components/Header';
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>TodoList</h1>
+        </header>
+    )
+}
+
+const headerStyle = {
+    background: 'blue',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px',
+}
+
+
+
+export default Header;
